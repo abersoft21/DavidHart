8699825ba5feb1a67baf17e460f48b56dc8524da
abersoft21 <81194422+abersoft21@users.noreply.github.com>
2021-03-30 17:37:15 +0100

    uploaded 30 Mar 21

---
+++ Share Brokering - David Hart/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Share_Brokering___David_Hart
+{
+    partial class Trader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnUploadData = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnUploadData
+            // 
+            this.btnUploadData.Location = new System.Drawing.Point(12, 12);
+            this.btnUploadData.Name = "btnUploadData";
+            this.btnUploadData.Size = new System.Drawing.Size(125, 23);
+            this.btnUploadData.TabIndex = 0;
+            this.btnUploadData.Text = "Upload Share Data";
+            this.btnUploadData.UseVisualStyleBackColor = true;
+            this.btnUploadData.Click += new System.EventHandler(this.btnUploadData_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(12, 60);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // Trader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 139);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnUploadData);
+            this.Name = "Trader";
+            this.Text = "Share Trader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnUploadData;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Share Brokering - David Hart/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entities;
+using BLL_VirtualTrader;
+
+namespace Share_Brokering___David_Hart
+{
+    public partial class Trader : Form
+    {
+        public Trader()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUploadData_Click(object sender, EventArgs e)
+        {
+            string fileStr = OpenFileUploader();
+
+            if (fileStr.Length > 0)
+            {
+                ConvertAndProcessFile(fileStr);
+            }
+        }
+
+        private void ConvertAndProcessFile(string fileStr)
+        {
+            Trading trade = new Trading(fileStr);
+            trade.Start();
+            lblOutput.Text = trade.Result();
+        }
+
+        private string OpenFileUploader()
+        {
+            Stream fileStream = null;
+            string fileString = string.Empty;
+
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "Open CSV File";
+
+            // can add actual csv's later; allow unknown extensions option for now. 
+            openDialog.Filter = "Text files|*.txt|All Types|*.*";
+            openDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((fileStream = openDialog.OpenFile()) != null)
+                    {
+                        byte[] buffer = new byte[fileStream.Length];
+
+                        // for scope for reading share values for a month, unlikely to exceed a few Kb but if it were several shares, maybe consider stream chunks, buffering for performance?
+                        using (StreamReader reader = new StreamReader(fileStream))
+                        {
+                            fileString = reader.ReadToEnd();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                     MessageBox.Show("Error: Could not read file " + ex.Message);
+                }
+            }
+            return fileString;
+        }
+    }
+}
