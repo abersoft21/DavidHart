8b482aff1819680d14c7f22bd8a661de3b262b47
abersoft21 <81194422+abersoft21@users.noreply.github.com>
2021-03-30 17:48:13 +0100

    Add files via upload

---
+++ Entities/StockTrade.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class StockTrade
+    {
+        public string StockCode
+        {
+            get;
+            set;
+        }
+
+        public int BuyWeek
+        {
+            get;
+            set;
+        }
+        public decimal BuyPrice
+        {
+            get;
+            set;
+        }
+        public int SellWeek
+        {
+            get;
+            set;
+        }
+        public decimal SellPrice
+        {
+            get;
+            set;
+        }
+
+        public decimal ROI
+        {
+            get;
+            set;
+        }
+    }
+}
